fa4a69b94ac5156a98ab6a5342251e361f3acbbb
amberyuy <yuywang@bu.edu>
2020-12-05 15:04:04 -0500

    add zuul

---
+++ homepage-zuul/src/main/java/com/minibackend/homepage/ZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.minibackend.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关应用程序
+ * EnableZuulProxy: 标识当前的应用是 Zuul Server
+ * SpringCloudApplication: 用于简化配置的组合注解,
+ *  组合了 @SpringBootApplication + @EnableDiscoveryClient + @EnableCircuitBreaker
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
